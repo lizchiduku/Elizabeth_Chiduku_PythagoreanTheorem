057c7955408bc0983e7a3f64eb564ab31845e3a2
lizchiduku <50470205+lizchiduku@users.noreply.github.com>
2019-06-17 17:13:22 -0500

    Update PythagoreanTest.java

--- PythagoreanTest.java
+++ PythagoreanTest.java
@@ -1,7 +1,10 @@
 public class PythagoreanTest {
 	public static void main(String[] args) {
 		Pythagorean pythag = new Pythagorean();
+		
+		//given the value of the adjacent side and opposite side//
+		
 		double hypo = pythag.calculateHypotenuse(5,6);
-		System.out.println(hypo);
+		System.out.println(hypotenuse);
 	}
 }
