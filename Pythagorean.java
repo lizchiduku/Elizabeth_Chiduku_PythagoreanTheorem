414d421b73d45bbe133e033e8d23411220e3b7b1
lizchiduku <50470205+lizchiduku@users.noreply.github.com>
2019-06-17 17:17:39 -0500

    Update Pythagorean.java

--- Pythagorean.java
+++ Pythagorean.java
@@ -1,6 +1,9 @@
 import java.lang.Math;
 public class Pythagorean {
 	public double calculateHypotenuse(int legA, int legB) {
+		
+//Hyp = Opp^2 + Adj^2. below, the methode used is multiplying a number by itself, then summing both results to get Hypotunese value//
+		
 		double hypoteneuse = Math.sqrt((legA * legA) + (legB *legB));
 		return hypotenuse;
 	}
